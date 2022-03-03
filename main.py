f0fe9bdac7c9e9aff7e02eb15ee42bbedef05c03
Otman LAHRECHE <laotman.47@gmail.com>
2022-03-03 15:50:53 +0100

    updates

--- main.py
+++ main.py
@@ -10,11 +10,24 @@ class MainUi(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainUi, self).__init__()
         uic.loadUi('ui/main.ui', self)
+        self.epsp = self.findChild(QtWidgets.QLabel, "label")
+        self.choisir = self.findChild(QtWidgets.QLabel, "label_2")
+
+        self.choisir.setContentsMargins(40, 0, 5, 0)
+
+        self.grid = self.findChild(QtWidgets.QGridLayout, "gridLayout")
         self.urgence = self.findChild(QtWidgets.QPushButton, "pushButton")
         self.dentist = self.findChild(QtWidgets.QPushButton, "pushButton_3")
         self.radio = self.findChild(QtWidgets.QPushButton, "pushButton_2")
         self.labo = self.findChild(QtWidgets.QPushButton, "pushButton_4")
         self.pharm = self.findChild(QtWidgets.QPushButton, "pushButton_5")
+        self.grid.addWidget(self.urgence, 1, 1)
+        self.grid.addWidget(self.dentist, 1, 2)
+        self.grid.addWidget(self.radio, 1, 3)
+        self.grid.addWidget(self.labo, 2, 1)
+        self.grid.addWidget(self.pharm, 2, 2)
+
+
 
         self.urgence.clicked.connect(self.urg)
         self.dentist.clicked.connect(self.den)
