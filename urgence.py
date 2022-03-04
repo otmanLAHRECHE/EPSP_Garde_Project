3d3b2aa664fd97ba239ac7a501e049414d0bf62b
Otman LAHRECHE <laotman.47@gmail.com>
2022-03-04 13:49:38 +0100

    updates

---
+++ urgence.py
@@ -0,0 +1,23 @@
+from PyQt5 import uic, QtWidgets
+from PyQt5.QtWidgets import *
+import sys
+
+
+class UrgenceMainUi(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(UrgenceMainUi, self).__init__()
+        uic.loadUi('ui/urgence.ui', self)
+        self.tab = self.findChild(QtWidgets.QTabWidget, "tabWidget")
+
+        self.tab_gardes = QWidget()
+        self.tab_medcines = QWidget()
+
+        self.tab.addTab(self.tab_gardes, "Listes de Garde")
+        self.tab.addTab(self.tab_medcines, "Medcines")
+
+        self.show()
+
+
+app = QApplication(sys.argv)
+window = UrgenceMainUi()
+app.exec_()
