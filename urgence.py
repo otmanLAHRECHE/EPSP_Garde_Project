a30e2fa0ab98439583c88b694198fd4e147fe781
Otman LAHRECHE <laotman.47@gmail.com>
2022-03-05 15:09:24 +0100

    updates

--- urgence.py
+++ urgence.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 from PyQt5 import uic, QtWidgets, QtSql, QtCore
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import *
@@ -8,9 +10,6 @@ class UrgenceMainUi(QtWidgets.QMainWindow):
     def __init__(self):
         super(UrgenceMainUi, self).__init__()
         uic.loadUi('ui/urgence.ui', self)
-
-        self.con = QtSql.QSqlDatabase.addDatabase("QSQLITE")
-        self.con.setDatabaseName("database/sqlite.db")
 
         self.tab = self.findChild(QtWidgets.QTabWidget, "tabWidget")
 
@@ -28,51 +27,46 @@ class UrgenceMainUi(QtWidgets.QMainWindow):
         self.update.setIcon(QIcon("asstes/images/edit.png"))
         self.delete = self.findChild(QPushButton, "pushButton_2")
         self.delete.setIcon(QIcon("asstes/images/user-x.png"))
+        self.table = self.findChild(QTableWidget, "tableWidget")
+        self.table.setColumnWidth(0, 80)
+        self.table.setColumnWidth(1, 200)
+        self.table.setColumnWidth(2, 110)
+        self.loadUsers()
 
-        self.model = QtSql.QSqlTableModel()
-        self.model.setTable('health_worker')
-        self.model.setEditStrategy(QtSql.QSqlTableModel.OnFieldChange)
-        self.model.select()
-        self.model.setHeaderData(0, QtCore.Qt.Horizontal, "id")
-        self.model.setHeaderData(1, QtCore.Qt.Horizontal, "Nom et Prenom")
-        self.model.setHeaderData(2, QtCore.Qt.Horizontal, "Service")
-
-        self.table = self.findChild(QTableView, "tableView")
-
-        self.table.setModel(self.model)
-        self.i = self.model.rowCount()
-
-        self.add.clicked.connect(self.add_toDb())
-        self.update.clicked.connect(self.update_toDb())
-        self.delete.clicked.connect(self.delete_toDb())
+        self.add.clicked.connect(self.add_toDb)
 
     def alert_no_name(self):
         alert = QMessageBox()
+        alert.setWindowTitle("alert")
         alert.setText('Le champ de nom est vide!')
         alert.exec_()
 
     def add_toDb(self):
-        print(self.i)
         if self.medcinname.text() == "":
             self.alert_no_name()
         else:
-            self.model.insertRows(self.i, 1)
-            self.model.setData(self.model.index(self.i, 1), self.ui.lineEdit.text())
-            self.model.setData(self.model.index(self.i, 2), self.ui.lineEdit_2.text())
-            self.model.submitAll()
-            self.i += 1
+            connection = sqlite3.connect('database/sqlite.db')
+            print("connection")
+            cur = connection.cursor()
+            sql_q = "INSERT INTO health_worker (full_name,service) values (?,?)"
+            med = (self.medcinname.text(), 'medecin urgence')
+            cur.execute(sql_q, med)
+            connection.commit()
+            connection.close()
+            self.loadUsers()
 
-    def delete_toDb(self):
-        if self.table.currentIndex().row() > -1:
-            self.model.removeRow(self.table.currentIndex().row())
-            self.i -= 1
-            self.model.select()
-
-    def update_toDb(self):
-        if self.table.currentIndex().row() > -1:
-            record = self.model.record(self.ui.tableWidget.currentIndex().row())
-            record.setValue("Name", self.ui.lineEdit.text())
-            record.setValue("Surname", self.ui.lineEdit_2.text())
-            record.setValue("DOB", self.ui.dateEdit.text())
-            record.setValue("Phone", self.ui.lineEdit_3.text())
-            self.model.setRecord(self.ui.tableWidget.currentIndex().row(), record)
+    def loadUsers(self):
+        print("load users")
+        connection = sqlite3.connect('database/sqlite.db')
+        cur = connection.cursor()
+        sql_q = 'SELECT * FROM health_worker where service=?'
+        tablerow = 0
+        cur.execute(sql_q, ('medecin urgence',))
+        results = cur.fetchall()
+        for row in results:
+            print(row)
+            self.table.setItem(tablerow, 0, QtWidgets.QTableWidgetItem(row[0]))
+            self.table.setItem(tablerow, 1, QtWidgets.QTableWidgetItem(row[1]))
+            self.table.setItem(tablerow, 2, QtWidgets.QTableWidgetItem(row[2]))
+            tablerow += 1
+        connection.close()
