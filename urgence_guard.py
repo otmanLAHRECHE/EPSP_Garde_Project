717bc2d8365b2af83652224ef8c755140ea3ed0c
Otman LAHRECHE <laotman.47@gmail.com>
2022-03-07 11:53:09 +0100

    updates

---
+++ urgence_guard.py
@@ -0,0 +1,37 @@
+from PyQt5 import QtWidgets, uic
+
+
+class UrgenceGuardUi(QtWidgets.QMainWindow):
+    def __init__(self, month, year):
+        super(UrgenceGuardUi, self).__init__()
+        uic.loadUi('ui/guard_urgence.ui', self)
+
+        self.ttl = self.findChild(QtWidgets.QLabel, "label")
+        self.month = month
+        self.year = year
+        m = ""
+        if self.month == 1:
+            m = "janvier"
+        elif self.month == 2:
+            m = "février"
+        elif self.month == 3:
+            m = "mars"
+        elif self.month == 4:
+            m = "avril"
+        elif self.month == 5:
+            m = "mai"
+        elif self.month == 6:
+            m = "juin"
+        elif self.month == 7:
+            m = "juillet"
+        elif self.month == 8:
+            m = "août"
+        elif self.month == 9:
+            m = "septembre"
+        elif self.month == 10:
+            m = "octobre"
+        elif self.month == 11:
+            m = "novembre"
+        elif self.month == 12:
+            m = "décembre"
+        self.ttl.setText("Planing de garde urgence mois "+m+" de l année "+year)
