8d3d5aea1fd34c10e8096918e8d5f47f4a9e65d1
Otman LAHRECHE <laotman.47@gmail.com>
2022-03-08 10:13:04 +0100

    important updates

--- urgence_guard.py
+++ urgence_guard.py
@@ -1,6 +1,10 @@
+import datetime
+import sqlite3
+
 from PyQt5 import QtWidgets, uic
+from calendar import monthrange
 
-
+from PyQt5.QtWidgets import QTableWidgetItem
 
 
 class UrgenceGuardUi(QtWidgets.QMainWindow):
@@ -9,14 +13,15 @@ class UrgenceGuardUi(QtWidgets.QMainWindow):
         uic.loadUi('ui/guard_urgence.ui', self)
 
         self.ttl = self.findChild(QtWidgets.QLabel, "label")
+        self.table = self.findChild(QtWidgets.QTableWidget, "tableWidget")
+        self.table.setColumnWidth(2, 220)
+        self.table.setColumnWidth(3, 220)
 
         self.month = month
         self.year = year
-
-        print(self.month)
-        print(self.year)
-
-
+        self.num_days = monthrange(self.year, self.month)[1]
+        self.num_days = self.num_days - 1
+        print(self.num_days)
 
         if self.month == 1:
             m = "janvier"
@@ -43,4 +48,44 @@ class UrgenceGuardUi(QtWidgets.QMainWindow):
         elif self.month == 12:
             m = "décembre"
 
-        self.ttl.setText("Planing de garde urgence mois " + str(m) + " de l année " + str(self.year) + ":")
+        self.ttl.setText("Planing de garde urgence mois " + str(m) + "/" + str(self.year) + ":")
+        self.load_guards()
+
+    def load_guards(self):
+        print("load guard list")
+        """
+        connection = sqlite3.connect('database/sqlite.db')
+        cur = connection.cursor()
+        sql_q = 'SELECT * FROM guard where service=?'
+        tablerow = 0
+        cur.execute(sql_q, ('urgence',))
+        results = cur.fetchall()
+        """
+        for row in range(self.num_days):
+            print(row)
+            day = row + 1
+            x = datetime.datetime(self.year, self.month, day)
+            m = ""
+            if x.strftime("%A") == "Saturday":
+                m = "Samedi"
+            elif x.strftime("%A") == "Sunday":
+                m = "Dimanche"
+            elif x.strftime("%A") == "Monday":
+                m = "Lundi"
+            elif x.strftime("%A") == "Tuesday":
+                m = "Mardi"
+            elif x.strftime("%A") == "Wednesday":
+                m = "Mercredi"
+            elif x.strftime("%A") == "Thursday":
+                m = "Jeudi"
+            elif x.strftime("%A") == "Friday":
+                m = "Vendredi"
+
+            self.table.setItem(row, 0, QTableWidgetItem(m))
+            self.table.setItem(row, 1, QTableWidgetItem(str(day) + "/" + str(self.month) + "/" + str(self.year)))
+            """
+            self.table_gardes.setItem(row, 2, QTableWidgetItem(str(row[2])))
+            self.table_gardes.setItem(row, 3, QTableWidgetItem(row[3]))
+            buttons = Buttons()
+            self.table_gardes.setCellWidget(tablerow, 4, buttons)
+            """
