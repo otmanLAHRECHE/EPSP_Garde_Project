f30a99cfa7db10bcaa4f1f110558ae7769e759c4
Otman LAHRECHE <laotman.47@gmail.com>
2022-03-13 09:22:56 +0100

    important updates

--- threads.py
+++ threads.py
@@ -621,3 +621,89 @@ class ThreadGuardDentiste(QThread):
         connection.close()
         print(self.data)
         self._signal_result.emit(self.data)
+
+
+class ThreadConsultationDentiste(QThread):
+    _signal = pyqtSignal(int)
+    _signal_result = pyqtSignal(list)
+
+    def __init__(self, num_days, month, year):
+        super(ThreadConsultationDentiste, self).__init__()
+        self.num_days = num_days
+        self.month = month
+        self.year = year
+        self.data = [("Jours", "Date", "De 08h:00 à 16h:00", "De 16h:00 à 20h:00")]
+
+    def __del__(self):
+        self.terminate()
+        self.wait()
+
+    def run(self):
+        connection = sqlite3.connect('database/sqlite.db')
+        cur = connection.cursor()
+
+        for row in range(self.num_days):
+
+            prog = row * 100 / self.num_days
+            day = row + 1
+            x = datetime.datetime(self.year, self.month, day)
+            light = "Dr/ "
+            night = "Dr/ "
+            m = ""
+            if x.strftime("%A") == "Saturday":
+                m = "Samedi"
+                light = ""
+                night = ""
+            elif x.strftime("%A") == "Sunday":
+                m = "Dimanche"
+            elif x.strftime("%A") == "Monday":
+                m = "Lundi"
+            elif x.strftime("%A") == "Tuesday":
+                m = "Mardi"
+            elif x.strftime("%A") == "Wednesday":
+                m = "Mercredi"
+            elif x.strftime("%A") == "Thursday":
+                m = "Jeudi"
+            elif x.strftime("%A") == "Friday":
+                m = "Vendredi"
+                light = ""
+                night = ""
+
+            if self.month / 10 >= 1:
+                if day / 10 >= 1:
+                    date_day = str(day) + "/" + str(self.month) + "/" + str(self.year)
+                else:
+                    date_day = str(0) + str(day) + "/" + str(self.month) + "/" + str(self.year)
+            else:
+                if day / 10 >= 1:
+                    date_day = str(day) + "/" + str(0) + str(self.month) + "/" + str(self.year)
+                else:
+                    date_day = str(0) + str(day) + "/" + str(0) + str(self.month) + "/" + str(self.year)
+
+            sql_q = 'SELECT health_worker.full_name FROM health_worker INNER JOIN consultation ON health_worker.worker_id = consultation.consultent_id where service=? and consultation.periode =? and consultation.d =? and consultation.m =? and consultation.y =?'
+            cur.execute(sql_q, ('dentiste', 'light', day, self.month, self.year))
+            results_light = cur.fetchall()
+
+            sql_q = 'SELECT health_worker.full_name FROM health_worker INNER JOIN consultation ON health_worker.worker_id = consultation.consultent_id where service=? and consultation.periode =? and consultation.d =? and consultation.m =? and consultation.y =?'
+            cur.execute(sql_q, ('dentiste', 'night', day, self.month, self.year))
+            results_night = cur.fetchall()
+
+
+            if results_light:
+                rl = results_light[0]
+                light = light + str(rl[0])
+
+            if results_night:
+                rn = results_night[0]
+                night = night + str(rn[0])
+
+            data_day = (m, date_day, light, night)
+
+            self.data.append(data_day)
+
+            time.sleep(0.3)
+            self._signal.emit(int(prog))
+
+        connection.close()
+        print(self.data)
+        self._signal_result.emit(self.data)
