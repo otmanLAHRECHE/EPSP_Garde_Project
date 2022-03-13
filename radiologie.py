b63900e096bc3f24499bdd3576c8efb387b0f055
Otman LAHRECHE <laotman.47@gmail.com>
2022-03-13 10:51:45 +0100

    important updates

--- radiologie.py
+++ radiologie.py
@@ -2,38 +2,26 @@ import sqlite3
 
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QWidget, QLineEdit, QPushButton, QTableWidget, QMessageBox, QTableWidgetItem, qApp
+from PyQt5.QtWidgets import QLineEdit, QPushButton, QTableWidget, QMessageBox, QTableWidgetItem, qApp
 
-from dentiste_consultation import DentisteConsultationUi
-from dentiste_guard import DentisteGuardUi
 from dialogs import Add_new_month, Update_worker_dialog
-import export_dentiste_consultation
-import export_dentiste_guard
-from tools import get_workers_count, get_guard_months_count, get_consultation_months_count
-from urgence_guard import UrgenceGuardUi
+from tools import get_workers_count, get_guard_months_count
 from widgets import Buttons
 
 
-class DentisteMainUi(QtWidgets.QMainWindow):
+class RadiologieMainUi(QtWidgets.QMainWindow):
     def __init__(self):
-        super(DentisteMainUi, self).__init__()
-        uic.loadUi('ui/dentiste.ui', self)
+        super(RadiologieMainUi, self).__init__()
+        uic.loadUi('ui/urgence.ui', self)
 
         self.tab = self.findChild(QtWidgets.QTabWidget, "tabWidget")
 
-        self.tab.setTabText(0, "Listes des gardes")
-        self.tab.setTabText(1, "Liste des consultations")
-        self.tab.setTabText(2, "Liste des dentistes")
 
-        self.dentistename = self.findChild(QLineEdit, "lineEdit")
+        self.tab.setTabText(0, "Listes de garde")
+        self.tab.setTabText(1, "Agents de radio")
+        self.medcinname = self.findChild(QLineEdit, "lineEdit")
         self.add_garde = self.findChild(QPushButton, "pushButton_4")
         self.add_garde.setIcon(QIcon("asstes/images/plus.png"))
-
-        self.add_consultation = self.findChild(QPushButton, "pushButton_5")
-        self.add_consultation.setIcon(QIcon("asstes/images/plus.png"))
-
-        self.add_consultation = self.findChild(QPushButton, "pushButton_5")
-        self.add_consultation.setIcon(QIcon("asstes/images/plus.png"))
 
         self.add = self.findChild(QPushButton, "pushButton")
         self.add.setIcon(QIcon("asstes/images/plus.png"))
@@ -45,6 +33,7 @@ class DentisteMainUi(QtWidgets.QMainWindow):
         self.table.setColumnWidth(0, 80)
         self.table.setColumnWidth(1, 200)
         self.table.setColumnWidth(2, 110)
+        self.table.setColumnWidth(3, 110)
 
         self.table_gardes = self.findChild(QTableWidget, "tableWidget_2")
 
@@ -55,24 +44,13 @@ class DentisteMainUi(QtWidgets.QMainWindow):
         self.table_gardes.setColumnWidth(4, 220)
         self.table_gardes.hideColumn(0)
 
-        self.table_consultation = self.findChild(QTableWidget, "tableWidget_3")
-
-        self.table_consultation.setColumnWidth(0, 150)
-        self.table_consultation.setColumnWidth(1, 150)
-        self.table_consultation.setColumnWidth(2, 150)
-        self.table_consultation.setColumnWidth(3, 180)
-        self.table_consultation.setColumnWidth(4, 220)
-        self.table_consultation.hideColumn(0)
-
         self.loadGuardMonths()
-        self.loadConsultationMonths()
         self.loadUsers()
 
         self.add.clicked.connect(self.add_toDb)
         self.delete.clicked.connect(self.deleteUser)
         self.update.clicked.connect(self.updateUser)
         self.add_garde.clicked.connect(self.add_grd)
-        self.add_consultation.clicked.connect(self.add_cns)
 
     def alert_(self, message):
         alert = QMessageBox()
@@ -81,28 +59,28 @@ class DentisteMainUi(QtWidgets.QMainWindow):
         alert.exec_()
 
     def add_toDb(self):
-        med_count = get_workers_count("dentiste")[0]
-        if self.dentistename.text() == "":
-            message = 'Le champ de nom est vide!'
+        med_count = get_workers_count("radio")[0]
+        if self.medcinname.text() == "" :
+            message = 'Le champ de nom est vide! ou groupe na pas etre choisir'
             self.alert_(message)
 
         elif med_count[0] > 25:
-            message = 'Maximum nombre des dentistes, supremer un dentiste'
+            message = 'Maximum nombre des agents, supremer un agent'
             self.alert_(message)
         else:
             connection = sqlite3.connect('database/sqlite.db')
             cur = connection.cursor()
             sql_q = "INSERT INTO health_worker (full_name,service) values (?,?)"
-            med = (self.dentistename.text(), 'dentiste')
+            med = (self.medcinname.text(), 'radio')
             cur.execute(sql_q, med)
             connection.commit()
             connection.close()
-            self.dentistename.setText("")
+            self.medcinname.setText("")
             self.loadUsers()
 
     def add_grd(self):
 
-        guards_months_count = get_guard_months_count("dentiste")[0]
+        guards_months_count = get_guard_months_count("radio")[0]
 
         dialog = Add_new_month()
         if dialog.exec() == QtWidgets.QDialog.Accepted:
@@ -142,7 +120,7 @@ class DentisteMainUi(QtWidgets.QMainWindow):
                 elif dialog.month.currentIndex() == 11:
                     m = 12
 
-                guard_m = (m, int(dialog.year.text()), 'dentiste')
+                guard_m = (m, int(dialog.year.text()), 'radio')
                 print(guard_m)
 
                 cur.execute(sql_q, guard_m)
@@ -165,7 +143,7 @@ class DentisteMainUi(QtWidgets.QMainWindow):
             self.table.setItem(row, 2, QTableWidgetItem(""))
             self.loadUsers()
         else:
-            message = 'Selectioner un dentiste'
+            message = 'Selectioner un agents'
             self.alert_(message)
 
     def updateUser(self):
@@ -188,7 +166,7 @@ class DentisteMainUi(QtWidgets.QMainWindow):
                     self.loadUsers()
 
         else:
-            message = 'Selectioner un dentiste'
+            message = 'Selectioner un agent'
             self.alert_(message)
 
     def loadUsers(self):
@@ -197,7 +175,7 @@ class DentisteMainUi(QtWidgets.QMainWindow):
         cur = connection.cursor()
         sql_q = 'SELECT * FROM health_worker where service=?'
         tablerow = 0
-        cur.execute(sql_q, ('dentiste',))
+        cur.execute(sql_q, ('radio',))
         results = cur.fetchall()
         for row in results:
             self.table.setItem(tablerow, 0, QTableWidgetItem(str(row[0])))
@@ -215,7 +193,7 @@ class DentisteMainUi(QtWidgets.QMainWindow):
         cur = connection.cursor()
         sql_q = 'SELECT * FROM guard_mounth where service=?'
         tablerow = 0
-        cur.execute(sql_q, ('dentiste',))
+        cur.execute(sql_q, ('radio',))
         results = cur.fetchall()
         for row in results:
             self.table_gardes.setRowHeight(tablerow, 70)
@@ -296,9 +274,11 @@ class DentisteMainUi(QtWidgets.QMainWindow):
 
         y = int(y)
 
-        self.dentiste_guard_page = DentisteGuardUi(m, y)
-        self.dentiste_guard_page.show()
+        """
+        self.urgence_guard_page = urgence_guard.UrgenceGuardUi(m, y)
+        self.urgence_guard_page.show()
         self.close()
+        """
 
     def delete_g(self):
         clickme = qApp.focusWidget()
@@ -352,202 +332,8 @@ class DentisteMainUi(QtWidgets.QMainWindow):
 
         y = int(y)
 
-        self.next_page = export_dentiste_guard.ExportDentisteGuardUi(m, y)
+        """
+        self.next_page = export_urgence_planing.ExportUrgencePlaningUi(m, y)
         self.close()
         self.next_page.show()
-
-    def add_cns(self):
-
-        consultations_months_count = get_consultation_months_count("dentiste")[0]
-
-        dialog = Add_new_month()
-        if dialog.exec() == QtWidgets.QDialog.Accepted:
-            if dialog.year.text() == "":
-                message = "Entrer une valid année"
-                self.alert_(message)
-            elif consultations_months_count[0] > 13:
-                message = "Maximum liste des consultations, suprimrer des listes"
-                self.alert_(message)
-            else:
-                connection = sqlite3.connect('database/sqlite.db')
-                cur = connection.cursor()
-                sql_q = "INSERT INTO consultaion_mounth (m,y,service) values (?,?,?)"
-                m = 0
-                if dialog.month.currentIndex() == 0:
-                    m = 1
-                elif dialog.month.currentIndex() == 1:
-                    m = 2
-                elif dialog.month.currentIndex() == 2:
-                    m = 3
-                elif dialog.month.currentIndex() == 3:
-                    m = 4
-                elif dialog.month.currentIndex() == 4:
-                    m = 5
-                elif dialog.month.currentIndex() == 5:
-                    m = 6
-                elif dialog.month.currentIndex() == 6:
-                    m = 7
-                elif dialog.month.currentIndex() == 7:
-                    m = 8
-                elif dialog.month.currentIndex() == 8:
-                    m = 9
-                elif dialog.month.currentIndex() == 9:
-                    m = 10
-                elif dialog.month.currentIndex() == 10:
-                    m = 11
-                elif dialog.month.currentIndex() == 11:
-                    m = 12
-
-                consultation_m = (m, int(dialog.year.text()), 'dentiste')
-
-                cur.execute(sql_q, consultation_m)
-                connection.commit()
-                connection.close()
-                self.loadConsultationMonths()
-
-    def loadConsultationMonths(self):
-        connection = sqlite3.connect('database/sqlite.db')
-        cur = connection.cursor()
-        sql_q = 'SELECT * FROM consultaion_mounth where service=?'
-        tablerow = 0
-        cur.execute(sql_q, ('dentiste',))
-        results = cur.fetchall()
-        for row in results:
-            self.table_consultation.setRowHeight(tablerow, 70)
-            m = ""
-            if row[1] == 1:
-                m = "janvier"
-            elif row[1] == 2:
-                m = "février"
-            elif row[1] == 3:
-                m = "mars"
-            elif row[1] == 4:
-                m = "avril"
-            elif row[1] == 5:
-                m = "mai"
-            elif row[1] == 6:
-                m = "juin"
-            elif row[1] == 7:
-                m = "juillet"
-            elif row[1] == 8:
-                m = "août"
-            elif row[1] == 9:
-                m = "septembre"
-            elif row[1] == 10:
-                m = "octobre"
-            elif row[1] == 11:
-                m = "novembre"
-            elif row[1] == 12:
-                m = "décembre"
-
-            self.table_consultation.setItem(tablerow, 0, QTableWidgetItem(str(row[0])))
-            self.table_consultation.setItem(tablerow, 1, QTableWidgetItem(m))
-            self.table_consultation.setItem(tablerow, 2, QTableWidgetItem(str(row[2])))
-            self.table_consultation.setItem(tablerow, 3, QTableWidgetItem(row[3]))
-            buttons = Buttons()
-            self.table_consultation.setCellWidget(tablerow, 4, buttons)
-            buttons.print_garde.clicked.connect(self.print_cns)
-            buttons.edit_garde.clicked.connect(self.edit_cns)
-            buttons.delete_garde.clicked.connect(self.delete_cns)
-
-            tablerow += 1
-        self.table_consultation.setItem(tablerow, 0, QTableWidgetItem(""))
-        self.table_consultation.setItem(tablerow, 1, QTableWidgetItem(""))
-        self.table_consultation.setItem(tablerow, 2, QTableWidgetItem(""))
-        self.table_consultation.setItem(tablerow, 3, QTableWidgetItem(""))
-        self.table_consultation.removeCellWidget(tablerow, 4)
-        connection.close()
-
-    def edit_cns(self):
-        clickme = qApp.focusWidget()
-        index = self.table_consultation.indexAt(clickme.parent().pos())
-        row = index.row()
-        m = self.table_consultation.item(row, 1).text()
-        y = self.table_consultation.item(row, 2).text()
-        if m == "janvier":
-            m = 1
-        elif m == "février":
-            m = 2
-        elif m == "mars":
-            m = 3
-        elif m == "avril":
-            m = 4
-        elif m == "mai":
-            m = 5
-        elif m == "juin":
-            m = 6
-        elif m == "juillet":
-            m = 7
-        elif m == "août":
-            m = 8
-        elif m == "septembre":
-            m = 9
-        elif m == "octobre":
-            m = 10
-        elif m == "novembre":
-            m = 11
-        elif m == "décembre":
-            m = 12
-
-        y = int(y)
-
-        self.dentiste_consultation_page = DentisteConsultationUi(m, y)
-        self.dentiste_consultation_page.show()
-        self.close()
-
-    def delete_cns(self):
-        clickme = qApp.focusWidget()
-        index = self.table_gardes.indexAt(clickme.parent().pos())
-        row = index.row()
-        if row > -1:
-            id = self.table_gardes.item(row, 0).text()
-            connection = sqlite3.connect('database/sqlite.db')
-            cur = connection.cursor()
-            sql_q = 'DELETE FROM consultaion_mounth WHERE consultation_mounth_id=?'
-            cur.execute(sql_q, (id,))
-            connection.commit()
-            connection.close()
-            self.table.setItem(row, 0, QTableWidgetItem(""))
-            self.table.setItem(row, 1, QTableWidgetItem(""))
-            self.table.setItem(row, 2, QTableWidgetItem(""))
-            self.table.setItem(row, 3, QTableWidgetItem(""))
-            self.table.setItem(row, 4, QTableWidgetItem(""))
-            self.loadGuardMonths()
-
-    def print_cns(self):
-        clickme = qApp.focusWidget()
-        index = self.table_gardes.indexAt(clickme.parent().pos())
-        row = index.row()
-        m = self.table_gardes.item(row, 1).text()
-        y = self.table_gardes.item(row, 2).text()
-        if m == "janvier":
-            m = 1
-        elif m == "février":
-            m = 2
-        elif m == "mars":
-            m = 3
-        elif m == "avril":
-            m = 4
-        elif m == "mai":
-            m = 5
-        elif m == "juin":
-            m = 6
-        elif m == "juillet":
-            m = 7
-        elif m == "août":
-            m = 8
-        elif m == "septembre":
-            m = 9
-        elif m == "octobre":
-            m = 10
-        elif m == "novembre":
-            m = 11
-        elif m == "décembre":
-            m = 12
-
-        y = int(y)
-
-        self.next_page = export_dentiste_consultation.ExportDentisteConsultationUi(m, y)
-        self.close()
-        self.next_page.show()
-
+        """
