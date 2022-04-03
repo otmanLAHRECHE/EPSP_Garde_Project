eb7b35d95229390aafa300b3bfa3d298486797bc
Otman LAHRECHE <laotman.47@gmail.com>
2022-04-03 10:50:32 +0100

    important updates

--- urgence_inf_guard.py
+++ urgence_inf_guard.py
@@ -112,8 +112,6 @@ class UrgenceInfGuardUi(QtWidgets.QMainWindow):
             else:
                 a0.ignore()
         else:
-            self.next_page = urgence_inf.UrgenceInfUi()
-            self.next_page.show()
             self.close()
 
     def signal_accepted(self, progress):
